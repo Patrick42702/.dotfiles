ed6d2bbb09b08550c64b9cdb5ddfb35abe52cdd2
patrick <patrick42702@gmail.com>
2025-05-01 09:55:44 -0400

    Some small changes to my dotfiles config

--- nvim/.config/nvim/lua/patrick/lazy.lua
+++ nvim/.config/nvim/lua/patrick/lazy.lua
@@ -9,6 +9,15 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
+		"lervag/vimtex",
+		lazy = false, -- we don't want to lazy load VimTeX
+		-- tag = "v2.15", -- uncomment to pin to a specific release
+		init = function()
+			-- VimTeX configuration goes here, e.g.
+			vim.g.vimtex_view_method = "zathura"
+		end,
+	},
+	{
 		"stevearc/conform.nvim",
 		opts = {},
 	},
