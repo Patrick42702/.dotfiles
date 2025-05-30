8033b15d2b5ce1a90f199b4a280b75b42754aee0
Patrick42702 <patrick42702@yahoo.com>
2025-05-30 08:42:03 -0400

    Moved my lazy plugins to another folder
    Moved my lazy plugins to another folder in their own files for a more
    modular and less bloated lazy.lua config file

---
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyo-night",
+    lazy = false,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+  },
+}
