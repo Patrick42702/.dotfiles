d38b34f4e7920577101baa77327fc66e43e34147
patrick <patrick42702@gmail.com>
2025-08-09 00:10:08 -0400

    Changed lazygit popup in toggleterm to tmux
    Changed lazygit to do a tmux popup vs. a toggleterm popup. Much more
    fluent

--- nvim/.config/nvim/after/plugin/toggleterm.lua
+++ nvim/.config/nvim/after/plugin/toggleterm.lua
@@ -1,53 +1,46 @@
 require("toggleterm").setup({
-  size = function(term)
-    if term.direction == "horizontal" then
-      return 15
-    elseif term.direction == "vertical" then
-      return vim.o.columns * 0.5 -- set to desired column width
-    end
-  end,
-  open_mapping = [[<c-\>]],
-  direction = "float", -- default, can be overridden per terminal
-  shade_terminals = true,
-  start_in_insert = true,
-  insert_mappings = true,
-  persist_size = false,
+	size = function(term)
+		if term.direction == "horizontal" then
+			return 15
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.5 -- set to desired column width
+		end
+	end,
+	open_mapping = [[<c-\>]],
+	direction = "float", -- default, can be overridden per terminal
+	shade_terminals = true,
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = false,
 })
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
-function _lazygit_toggle()
-  lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 
 local vertical_term = Terminal:new({
-  direction = "vertical",
-  hidden = true,
+	direction = "vertical",
+	hidden = true,
 })
 
 function _toggle_vertical_terminal()
-  vertical_term:toggle()
+	vertical_term:toggle()
 end
 
 vim.api.nvim_set_keymap(
-  "n",
-  "<leader>tv",
-  "<cmd>lua _toggle_vertical_terminal()<CR>",
-  { noremap = true, silent = true }
+	"n",
+	"<leader>tv",
+	"<cmd>lua _toggle_vertical_terminal()<CR>",
+	{ noremap = true, silent = true }
 )
 
 function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "jj", [[<C-\><C-n>]], opts)
-  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "jj", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
