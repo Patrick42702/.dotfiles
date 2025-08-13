3f14e94bb676b18b8a9ffc0799c63c7c40ba79ee
patrick muller <patrick42702@gmail.com>
2025-08-13 12:21:29 -0400

    Changed conform.lua to fix black and isort paths

--- nvim/.config/nvim/after/plugin/conform.lua
+++ nvim/.config/nvim/after/plugin/conform.lua
@@ -2,12 +2,17 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		-- Conform will run multiple formatters sequentially
-		python = { "isort", "black" },
+		python = {
+			{ command = "venv/bin/black" },
+			{ command = "venv/bin/isort" },
+		},
 		-- You can customize some of the format options for the filetype (:help conform.format)
 		rust = { "rustfmt", lsp_format = "fallback" },
 		-- Conform will run the first available formatter
 		javascript = { "prettierd", "prettier", stop_after_first = true },
-		cpp = { "astyle" },
+		cpp = {
+			{ command = "venv/bin/astyle" },
+		},
 	},
 })
 
